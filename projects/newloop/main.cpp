26423fe7d7d428b2c0a2a39b245e8e9f11d377e4
Hanck Edd <hanckeddison@gmail.com>
2022-07-30 14:34:03 -0700

    new loop system

--- projects/newloop/main.cpp
+++ projects/newloop/main.cpp
@@ -1,20 +1,23 @@
 //nr 1
-#include <iostream>
-#include <ctime>
+#include "../../HelloWorldEngine.h"
+void update();
+void draw();
+void input();
+
+auto* main_env = new Renderer(1, 64, 32, 12.0, 'q');
+auto main_win = Window(64, 32, 0, 0, 0, main_env);
+auto player = instance_create(1, 16, '@');
+
 int main(){
-	clock_t previous = clock();
-	double lag{};
-	while (true){
-		clock_t current = clock();
-		double elapsed = current - previous;
-		previous = current;
-		lag += elapsed;
-		//input here
-		while (lag > 100){
-			//update here
-			std::cout << elapsed << "\n";
-			lag -= 100;
-		}
-		//render here
-	}
+	main_env->game_loop(update, draw);
+	return 0;
+}
+
+void update(){
+	player->m_coordx+= 1;
+}
+
+void draw(){
+	main_win.clean();
+	instance_draw(main_win, player);
 }
